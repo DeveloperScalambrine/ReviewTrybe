2765a0d7e35feafc82960f33c1554ee8630910ed
Carlos Henrique <henriqueetec1987@gmail.com>
2025-04-28 15:59:34 -0300

    create a function to concat name and show elements in order changed

--- Project-Playground-functions/functions.js
+++ Project-Playground-functions/functions.js
@@ -8,4 +8,26 @@ const compareTrue = (valueOne, valueTwo) => {
     }
 }
 
-console.log(compareTrue(false, false));
+const calcArea = (base, height) => {
+    const triangle = (base * height) / 2;
+    return triangle;
+}
+
+const splitSentence = (phrase) => {
+   const wordWithSplit = phrase.split(' ');
+   return wordWithSplit;
+}
+
+const concatName = (arr) => {
+    if (arr.length === 0) {
+        return "Array is empty"
+    }
+    return {
+        last: arr[arr.length - 1],
+        first: arr[0]
+    }
+}
+
+console.log(concatName(['Carlos', 'Henrique']));
+
+    
