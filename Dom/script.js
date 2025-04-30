831a95bc42b5994ba85a818b16774bdbbdce7fca
Carlos Henrique <henriqueetec1987@gmail.com>
2025-04-30 01:52:11 -0300

    finished examples part II

---
+++ Dom/script.js
@@ -0,0 +1,44 @@
+
+// let parentItem = document.getElementById('parent')
+
+// let elementLi = document.createElement('li')
+
+// elementLi.innerHTML = "Create for DOM"
+
+// elementLi.appendChild(parentItem)
+
+// let title = document.getElementById('title')
+// title.innerText = 'Create element DOM Again'
+
+
+
+// let elmtPai = document.getElementById('pai');
+// let irmao = document.createElement('section');
+// irmao.id = 'irmaoDoElementoOndeVoceEsta';
+// irmao.textContent = 'Eu sou um irmão criado via JS';
+// elmtPai.appendChild(irmao);
+
+// const elmtMain = document.getElementById('elementoOndeVoceEsta')
+// let filho = document.createElement('section')
+// filho.id = 'filhoElementoOndeVoceEsta'
+// filho.textContent = "Sou filho criado via JS"
+// elmtMain.appendChild(filho)
+
+
+// const elmtNeto = document.getElementById('primeiroFilhoDoFilho')
+// let neto = document.createElement('section')
+// neto.id = 'netoDoelementoOndeVoceEsta'
+// neto.textContent = "Sou neto criado via JS"
+// elmtNeto.appendChild(neto)
+
+// let way = document.getElementById('netoDoelementoOndeVoceEsta')
+
+// let eu = way.parentNode.parentNode.parentNode.lastChild.previousElementSibling.previousElementSibling
+
+// console.log(eu);
+
+// let parent = document.getElementById('where-are-you')
+
+// let remove = document.getElementsByTagName('h2')[1]
+
+// parent.removeChild(remove)
