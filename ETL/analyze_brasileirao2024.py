cc917dc220b1a854651900e22de1b2e27dfe98f8
Carlos Henrique <henriqueetec1987@gmail.com>
2025-07-02 19:14:40 -0300

    Limpeza com os dados nulos

--- ETL/analyze_brasileirao2024.py
+++ ETL/analyze_brasileirao2024.py
@@ -2,4 +2,19 @@ import pandas as pd
 
 path_excel = 'File/Brasileiro_2024.xlsx'
 df = pd.read_excel(path_excel)
-print(df.head())
+
+#     Confirmando os registros retornados
+# print(f"Total de registros: {len(df)}")
+
+df = df.drop(index=0).reset_index(drop=True)
+
+# A informação aparece apenas uma vez por rodada, e as demais partidas não têm o número da rodada preenchido 
+# o que torna a análise inconsistente, para a coluna DATA - DIA tambem é verdadeiro
+# preencher valores nulos da coluna ROD com o último valor não nulo acima
+df['ROD'] = df['ROD'].fillna(method='ffill')
+df['DATA - DIA'] = df['DATA - DIA'].fillna(method='ffill')
+print(df.head(13))
+
+
+
+
