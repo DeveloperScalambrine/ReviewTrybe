f059623b47415d56f1bf2b09ce854a3bb9a84f4a
Carlos Henrique <henriqueetec1987@gmail.com>
2025-07-02 19:48:46 -0300

    Separação da coluna DATA - DIA para duas colunas DATA e DIA usando regex e ffill

--- ETL/analyze_brasileirao2024.py
+++ ETL/analyze_brasileirao2024.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
+# Caminho para o arquivo Excel
 path_excel = 'File/Brasileiro_2024.xlsx'
 df = pd.read_excel(path_excel)
 
-#     Confirmando os registros retornados
-# print(f"Total de registros: {len(df)}")
-
+# Remove a primeira linha (totalmente nula) e reinicia o índice
 df = df.drop(index=0).reset_index(drop=True)
 
+# Converte 'DATA - DIA' para string e extrai DATA e DIA
+df['DATA - DIA'] = df['DATA - DIA'].astype(str)
+df[['DATA', 'DIA']] = df['DATA - DIA'].str.extract(
+    r'(\d{2}/\d{2})\s+([a-zç]{3})', expand=True
+)
+
+df = df.drop(columns=['DATA - DIA'])
+
 # A informação aparece apenas uma vez por rodada, e as demais partidas não têm o número da rodada preenchido 
-# o que torna a análise inconsistente, para a coluna DATA - DIA tambem é verdadeiro
+# o que torna a análise inconsistente.
 # preencher valores nulos da coluna ROD com o último valor não nulo acima
-df['ROD'] = df['ROD'].fillna(method='ffill')
-df['DATA - DIA'] = df['DATA - DIA'].fillna(method='ffill')
-print(df.head(13))
+df['ROD'] = df['ROD'].ffill()
 
+df.at[0, 'DATA'] = '13/04'
+df.at[0, 'DIA']  = 'sab'
 
+# Preenche DATA e DIA apenas dentro de cada grupo de ROD
+df[['DATA', 'DIA']] = df.groupby('ROD')[['DATA', 'DIA']].ffill()
 
-
+print(df.head(10))
