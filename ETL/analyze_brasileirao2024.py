3bd2c55eddc0ed23f293d9a6852c6eb42e2f0a19
Carlos Henrique <henriqueetec1987@gmail.com>
2025-07-03 03:17:49 -0300

    Funcional e lendo as duas abas excel

--- ETL/analyze_brasileirao2024.py
+++ ETL/analyze_brasileirao2024.py
@@ -1,61 +1,63 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
-# # Caminho para o arquivo Excel
-# path_excel = 'File/Brasileiro_2024.xlsx'
-# df = pd.read_excel(path_excel)
+# Caminho para o arquivo Excel
+path_excel = "File/Tables.xlsx"
+all_able = pd.read_excel(path_excel, sheet_name=None)
+# df = pd.read_excel(path_excel, sheet_name="Rodadas-2024")
+# df_classificacao = pd.read_excel(path_excel, sheet_name="Classificação")
 
-# # Remove a primeira linha (totalmente nula) e reinicia o índice
-# df = df.drop(index=0).reset_index(drop=True)
+df = all_able["Rodadas-2024"]
+df_classificacao = all_able["Classificação"]
 
-# # Converte 'DATA - DIA' para string e extrai DATA e DIA
-# df['DATA - DIA'] = df['DATA - DIA'].astype(str)
-# df[['DATA', 'DIA']] = df['DATA - DIA'].str.extract(
-#     r'(\d{2}/\d{2})\s+([a-zç]{3})', expand=True
-# )
+# Exemplo: visualizar as 5 primeiras linhas de cada aba
+print(df.head())
+print(df_classificacao.head())
+print(len(df_classificacao) + len(df))
 
-# df = df.drop(columns=['DATA - DIA'])
+# Remove a primeira linha (totalmente nula) e reinicia o índice
+df = df.drop(index=0).reset_index(drop=True)
 
-# # A informação aparece apenas uma vez por rodada, e as demais partidas não têm o número da rodada preenchido 
-# # o que torna a análise inconsistente.
-# # preencher valores nulos da coluna ROD com o último valor não nulo acima
-# df['ROD'] = df['ROD'].ffill()
+# A informação aparece apenas uma vez por rodada, e as demais partidas não têm o número da rodada preenchido 
+# o que torna a análise inconsistente.
+# preencher valores nulos da coluna ROD com o último valor não nulo acima
+df['ROD'] = df['ROD'].ffill()
 
-# df.at[0, 'DATA'] = '13/04'
-# df.at[0, 'DIA']  = 'sab'
+df.at[0, 'DATA'] = '13/04'
+df.at[0, 'DIA']  = 'sab'
 
-# # Preenche DATA e DIA apenas dentro de cada grupo de ROD
-# df[['DATA', 'DIA']] = df.groupby('ROD')[['DATA', 'DIA']].ffill()
+# Preenche DATA e DIA apenas dentro de cada grupo de ROD
+df[['DATA', 'DIA']] = df.groupby('ROD')[['DATA', 'DIA']].ffill()
 
-# df['JOGO'] = df['JOGO'].astype(str)
-# df[['TimeMandante', 'UF_M', 'Gols_M', 'Gols_V', 'TimeVisitante', 'UF_V']] = df['JOGO'].str.extract(
-#     r'^(.*?)\s+([A-Z]{2})\s+(\d+)\s+x\s+(\d+)\s+(.*?)\s+([A-Z]{2})$'
-# )
+df['JOGO'] = df['JOGO'].astype(str)
+df[['TimeMandante', 'UF_M', 'Gols_M', 'Gols_V', 'TimeVisitante', 'UF_V']] = df['JOGO'].str.extract(
+    r'^(.*?)\s+([A-Z]{2})\s+(\d+)\s+x\s+(\d+)\s+(.*?)\s+([A-Z]{2})$'
+)
 
-# mask_invalid = df[['TimeMandante', 'UF_M', 'Gols_M', 'Gols_V', 'TimeVisitante', 'UF_V']].isnull().any(axis=1)
-# df_error_extract = df[mask_invalid]
+mask_invalid = df[['TimeMandante', 'UF_M', 'Gols_M', 'Gols_V', 'TimeVisitante', 'UF_V']].isnull().any(axis=1)
+df_error_extract = df[mask_invalid]
 
-# df['GolMandante'] = df['TimeMandante'].str.strip() + ' ' + df['UF_M'] + ' - ' + df['Gols_M']
-# df['GolVisitante'] = df['TimeVisitante'].str.strip() + ' ' + df['UF_V'] + ' - ' + df['Gols_V']
+df['GolMandante'] = df['TimeMandante'].str.strip() + ' ' + df['UF_M'] + ' - ' + df['Gols_M']
+df['GolVisitante'] = df['TimeVisitante'].str.strip() + ' ' + df['UF_V'] + ' - ' + df['Gols_V']
 
-#     #  Pequena analise filtrando por equipe e rodada
-# # df_filter_2rod = df[df['ROD'].isin(['1ª', '2ª', '3ª'])]
-# df['ROD_NUM'] = df['ROD'].str.extract(r'(\d+)').astype(int)
+    #  Pequena analise filtrando por equipe e rodada
+# df_filter_2rod = df[df['ROD'].isin(['1ª', '2ª', '3ª'])]
+df['ROD_NUM'] = df['ROD'].str.extract(r'(\d+)').astype(int)
 
-# # Filtra do início até a segunda rodada
-# df_filter_2rod = df[df['ROD_NUM'] <= 10]
+# Filtra do início até a segunda rodada
+df_filter_2rod = df[df['ROD_NUM'] <= 10]
 
-# principal = df_filter_2rod[df_filter_2rod['GolMandante'].str.contains(r'^São Paulo SP', regex=True)]
+principal = df_filter_2rod[df_filter_2rod['GolMandante'].str.contains(r'^São Paulo SP', regex=True)]
 
-# visitor = df_filter_2rod[df_filter_2rod['GolVisitante'].str.contains(r'^São Paulo SP', regex=True)]
+visitor = df_filter_2rod[df_filter_2rod['GolVisitante'].str.contains(r'^São Paulo SP', regex=True)]
 
-# gols_principal = principal['GolMandante'].str.extract(r' - (\d+)$').astype(int).sum().values[0]
-# gols_visitor = visitor['GolVisitante'].str.extract(r' - (\d+)$').astype(int).sum().values[0]
+gols_principal = principal['GolMandante'].str.extract(r' - (\d+)$').astype(int).sum().values[0]
+gols_visitor = visitor['GolVisitante'].str.extract(r' - (\d+)$').astype(int).sum().values[0]
 
-# total_gols_sp = gols_principal + gols_visitor
-# df.to_excel('File/Brasileiro_2024_limpo.xlsx', index=False)
-# print("📁 Arquivo salvo como 'File/Brasileiro_2024_limpo.xlsx'")
+total_gols_sp = gols_principal + gols_visitor
+df.to_excel('File/Brasileiro_2024_limpo.xlsx', index=False)
+print("📁 Arquivo salvo como 'File/Brasileiro_2024_limpo.xlsx'")
 
 #     # Gerando Grafico
 
@@ -109,25 +111,3 @@
 # # plt.savefig('img/grafico_sao_paulo_sp.png', dpi=300)
 # # print("✅ Gráfico salvo como 'grafico_sao_paulo_sp.png'")
 
-import pandas as pd
-
-# Caminho para o arquivo Excel com várias abas
-arquivo_excel = "File/Tables.xlsx"
-
-# Lê todas as abas (retorna um dicionário)
-todas_abas = pd.read_excel(arquivo_excel, sheet_name=None)
-
-# Lista os nomes das abas disponíveis
-print("Abas disponíveis:", todas_abas.keys())
-
-# Acessar uma aba específica por nome
-# df_rodadas = pd.read_excel(arquivo_excel, sheet_name="Rodadas-2024")
-# df_classificacao = pd.read_excel(arquivo_excel, sheet_name="Classificação")
-
-# # Ou, se estiver usando o dicionário:
-# # df_rodadas = todas_abas["Rodadas-2024"]
-# # df_classificacao = todas_abas["Classificação"]
-
-# # Exemplo: visualizar as 5 primeiras linhas de cada aba
-# print(df_rodadas.head())
-# print(df_classificacao.head())
